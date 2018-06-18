0f4bed284d30f3a933c6e58ff25e604326ba9476
Aveek <nsaveek@gmail.com>
2018-06-18 17:04:36 +0800

    calling and getting response using Retrofit ~ kotlin-java

--- app/src/kotlin/java/network/aveek/com/networkconnection/core/retrofit/GithubApiService.kt
+++ app/src/kotlin/java/network/aveek/com/networkconnection/core/retrofit/GithubApiService.kt
@@ -4,6 +4,7 @@ import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface GithubApiService {
